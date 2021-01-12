d5f570047ff1a00f674c8cab60c7ade368d5adff
geraldbathan24@gmail.com <geraldbathan24@gmail.com>
2021-01-12 13:17:43 +0800

    This is a sample Spring Boot

---
+++ src/test/java/net/code/java/HelloSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package net.code.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
